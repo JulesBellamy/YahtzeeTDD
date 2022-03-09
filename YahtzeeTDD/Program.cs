c6898d2bc8fa25ac5246aaefde59edb7bbb413ee
jules bellamy <bellamy.jules@hotmail.fr>
2022-03-09 16:54:35 +0100

    First commit

---
+++ YahtzeeTDD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace YahtzeeTDD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
